81e58eb0e735f29d3870bf4894e59fda815ca113
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-08-05 21:15:54 +0300

    Create Home

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,57 @@
+export default function Home(){
+    return (
+        <section id="content">
+        <div classNameName="container_12 top-1">
+          <div classNameName="grid_4 box-1"> <img src="images/page1-img1.jpg" alt=""/> <a href="#"><span classNameName="clr-1">Horse</span> Breeding</a> </div>
+          <div className="grid_4 box-1"> <img src="images/page1-img2.jpg" alt=""/> <a href="#"><span className="clr-1">Horse</span> Sale</a> </div>
+          <div className="grid_4 box-1"> <img src="images/page1-img3.jpg" alt=""/> <a href="#"><span className="clr-1">Horse</span> Events</a> </div>
+          <div className="grid_12">
+            <div className="line pad-1"></div>
+          </div>
+          <div className="grid_4">
+            <h2 className="p2">Welcome!</h2>
+            <img src="images/page1-img4.jpg" alt=""/>
+            <p className="text-1 top-2 p3">Lorem ipsum dolor sit amet</p>
+            <p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo.</p>
+            <a href="#" className="button top-3">Read More</a> </div>
+          <div className="grid_4">
+            <div className="left-1">
+              <h2 className="p4">Our Breeds</h2>
+              <div className="wrap">
+                <div className="number">1</div>
+                <p className="extra-wrap border-bot-1"><span className="clr-1">Lorem ipsum</span><br/>
+                  Horse Club is one of free website templates created by Template Monster team. </p>
+              </div>
+              <div className="wrap">
+                <div className="number">2</div>
+                <p className="extra-wrap border-bot-1"><span className="clr-1">Aliquyam erat</span><br/>
+                  This website template is optimized for 1280X1024 screen resolution. It is also XHTML & CSS valid.</p>
+              </div>
+              <div className="wrap">
+                <div className="number">3</div>
+                <p className="extra-wrap"><span className="clr-1">No sea takimata</span><br/>
+                  The PSD source files of this Horse Club are available for free for the registered members.</p>
+              </div>
+            </div>
+          </div>
+          <div className="grid_4">
+            <div className="left-2">
+              <h2 className="p4">Recent Events</h2>
+              <div className="wrap border-bot-1"> <img src="images/page1-img5.jpg" alt="" className="img-indent"/>
+                <p className="extra-wrap"/><span className="clr-1">At vero eos et accusam</span><br/>
+                  <a href="#" className="link">May 3, 2012</a><br>
+                  Justo duo dolores et rebum stet clita kasd g</br>ubergreno sea takimata sanctus consetetur sadipscing elitr.
+              </div>
+              <div className="wrap border-bot-1"> <img src="images/page1-img6.jpg" alt="" className="img-indent"/>
+                <p className="extra-wrap"/><span className="clr-1">At vero eos et accusam</span><br/>
+                  <a href="#" className="link">May 3, 2012</a><br/>
+                  Justo duo dolores et rebum stet clita kasd gubergreno sea takimata sanctus consetetur sadipscing elitr.
+              </div>
+            </div>
+          </div>
+          <div className="clear"></div>
+        </div>
+      </section>
+    )
+
+}
