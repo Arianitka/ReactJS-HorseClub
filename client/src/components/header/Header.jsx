50b7c2b154d1597715ab9fe3c07e10e9167aa436
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-08-05 21:15:43 +0300

    Update Header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,23 +1,23 @@
 export default function Header(){
     return (
         <header>
-  <div class="main">
+  <div className="main">
     <h1>Happy Horse</h1>
-    <div class="social-icons"> <span>Follow Us:</span>
-      <div> <a href="#" class="icon-3"></a> <a href="#" class="icon-2"></a> <a href="#" class="icon-1"></a> </div>
+    <div className="social-icons"> <span>Follow Us:</span>
+      <div> <a href="#" className="icon-3"></a> <a href="#" className="icon-2"></a> <a href="#" className="icon-1"></a> </div>
     </div>
-    <div class="clear"></div>
+    <div className="clear"></div>
   </div>
   <nav>
-  <ul class="menu">
-    <li class="current"><a href="index.html">Home</a></li>
+  <ul className="menu">
+    <li className="current"><a href="index.html">Home</a></li>
     <li><a href="breeding.html">Breeding</a></li>
     <li><a href="sale.html">Horse Sale</a></li>
     <li><a href="events.html">Events</a></li>
     <li><a href="gallery.html">Gallery</a></li>
     <li><a href="contacts.html">Contacts</a></li>
   </ul>
-  <div class="clear"></div>
+  <div className="clear"></div>
 </nav>
 </header>
 
