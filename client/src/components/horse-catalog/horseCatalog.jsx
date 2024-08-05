61f0be2c95e4c5538748302ca8b6105d389f63f7
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-08-06 02:10:06 +0300

    create  Catalog Page

---
+++ client/src/components/horse-catalog/horseCatalog.jsx
@@ -0,0 +1,37 @@
+export default function horseCatalog (){
+
+    return (
+        <section id="catalog-page">
+            <h1>All Games</h1>
+            <div class="allHorses">
+                <div class="allHorses-info">
+                    <img src="./images/avatar-1.jpg"/>
+                    <h6>Sport</h6>
+                    <h2>Fire</h2>
+                    <a href="#" class="details-button">Details</a>
+                </div>
+
+            </div>
+            <div class="allHorses">
+                <div class="allHorses-info">
+                    <img src="./images/avatar-1.jpg"/>
+                    <h6>Sport</h6>
+                    <h2>Lighitning</h2>
+                    <a href="#" class="details-button">Details</a>
+                </div>
+
+            </div>
+            <div class="allHorses">
+                <div class="allHorses-info">
+                    <img src="./images/avatar-1.jpg"/>
+                    <h6>Fun</h6>
+                    <h2>Horhe</h2>
+                    <a href="#" class="details-button">Details</a>
+                </div>
+            </div>
+
+
+            <h3 class="no-articles">No articles yet</h3>
+        </section>
+    )
+}
