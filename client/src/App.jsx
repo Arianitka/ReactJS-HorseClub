74e838b49a96db123d7bac9befba773426481eeb
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-08-06 12:28:23 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-import Header from "./components/header/Header";
+import { Routes, Route } from 'react-router-dom';
 
+import Header from "./components/header/Header";
+ 
 function App(){
     return (
         <div id="box">
             <Header />
+            <main id="main-content"> 
+                <Routes>
+            <Route path='/' element={<Home />}/>
+                </Routes>   
+
+            </main>
+
         </div>
     )
 }
+
+export default App
---
+++ client/src/components/edit-horse-game/EditHorseGame.jsx
@@ -0,0 +1,29 @@
+export default function EditHorseGame (){
+    return (
+        <section id="edit-page" class="auth">
+        <form id="edit">
+            <div class="container">
+
+                <h1>Edit Horse Game</h1>
+                <label for="leg-title">Horse Game Name</label>
+                <input type="text" id="title" name="title" value=""/>
+
+                <label for="category">Category:</label>
+                <input type="text" id="category" name="category" value=""/>
+
+                <label for="levels">Competition level:</label>
+                <input type="number" id="maxLevel" name="maxLevel" min="1" value=""/>
+
+                <label for="game-img">Image:</label>
+                <input type="text" id="imageUrl" name="imageUrl" value=""/>
+
+                <label for="summary">Summary:</label>
+                <textarea name="summary" id="summary"></textarea>
+                <input class="btn submit" type="submit" value="Edit Game"/>
+
+            </div>
+        </form>
+    </section>
+
+    )
+}
