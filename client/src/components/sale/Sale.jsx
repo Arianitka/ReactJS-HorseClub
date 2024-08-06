19b897bd78ec0a6d5257073ac017ccf2bd6a0ee7
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-08-06 12:51:41 +0300

    create sales page

---
+++ client/src/components/sale/Sale.jsx
@@ -0,0 +1,49 @@
+export default function Sale(){
+
+    return (
+        <section id="content">
+  <div className="container_12 top-4">
+    <div className="grid_4">
+      <h2>Special Note</h2>
+      <div className="box-4"><img src="images/page3-img4.png" alt=""/><span className="extra-wrap text-1">Consetetur sadipscing elitr sed diam nonumy.</span></div>
+      <p className="p1">Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo.</p>
+      <h2 className="p3">Special Note</h2>
+      <ul className="list-2">
+        <li><a href="#">Magna aliquyam erat sed diam</a></li>
+        <li><a href="#">At vero eos et accusam</a></li>
+        <li><a href="#">Stet clita kasd gubergren sea takimata</a></li>
+        <li><a href="#">Lorem ipsum dolor sit amet</a></li>
+        <li><a href="#">Consetetur sadipscing elitr sed</a></li>
+        <li><a href="#">Diam nonumy eirmod</a></li>
+        <li><a href="#">Tempor invidunt ut labore et dolore</a></li>
+        <li><a href="#">Magna aliquyam erat, sed diam</a></li>
+        <li><a href="#">At vero eos et accusam</a></li>
+      </ul>
+    </div>
+    <div className="grid_8">
+      <div className="left-1">
+        <h2 className="p2">Breeding Stallions</h2>
+        <div className="wrap box-5">
+          <div> <img src="images/page3-img1.jpg" alt=""/>
+            <p className="text-1 top-2 p3">Consete sadipscing</p>
+            <p className="clr-1 p3">Nam liber tepor soluta nobis eleifed option congue nihil imperdiet </p>
+            <p>Doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam </p>
+            <a href="#" className="button top-3">Read More</a> </div>
+          <div> <img src="images/page3-img2.jpg" alt=""/>
+            <p className="text-1 top-2 p3">Elitr sed diam</p>
+            <p className="clr-1 p3">Mibh euismod tincidunt ut laoreet dolore agna aliquam erat volutpat. </p>
+            <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+            <a href="#" className="button top-3">Read More</a> </div>
+          <div className="last"> <img src="images/page3-img3.jpg" alt=""/>
+            <p className="text-1 top-2 p3">Rirmod tempor</p>
+            <p className="clr-1 p3">Duis autem vel eum iriure dolor in hendrerit in vulptate velit esse.</p>
+            <p>Consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim quiandit praesent zzril.</p>
+            <a href="#" className="button top-3">Read More</a> </div>
+        </div>
+      </div>
+    </div>
+    <div className="clear"></div>
+  </div>
+</section>
+    )
+}
