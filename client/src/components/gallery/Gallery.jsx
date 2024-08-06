4e9e402113fd3e43dbefb133f095255be8d9d712
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-08-06 12:51:25 +0300

    create gallery page

---
+++ client/src/components/gallery/Gallery.jsx
@@ -0,0 +1,29 @@
+export default function Gallery(){
+    return (
+        <section id="content">
+        <div className="container_12 top-1">
+          <div className="grid_12">
+            <div className="pag">
+              <div className="img-pags">
+                <ul>
+                  <li><a href="#"><span><img src="images/gallery-1.jpg" alt=""/></span></a></li>
+                  <li><a href="#"><span><img src="images/gallery-2.jpg" alt=""/></span></a></li>
+                  <li><a href="#"><span><img src="images/gallery-3.jpg" alt=""/></span></a></li>
+                  <li><a href="#"><span><img src="images/gallery-4.jpg" alt=""/></span></a></li>
+                  <li><a href="#"><span><img src="images/gallery-5.jpg" alt=""/></span></a></li>
+                  <li><a href="#"><span><img src="images/gallery-6.jpg" alt=""/></span></a></li>
+                  <li><a href="#"><span><img src="images/gallery-7.jpg" alt=""/></span></a></li>
+                  <li><a href="#"><span><img src="images/gallery-8.jpg" alt=""/></span></a></li>
+                  <li><a href="#"><span><img src="images/gallery-9.jpg" alt=""/></span></a></li>
+                  <li><a href="#"><span><img src="images/gallery-10.jpg" alt=""/></span></a></li>
+                  <li><a href="#"><span><img src="images/gallery-11.jpg" alt=""/></span></a></li>
+                  <li><a href="#"><span><img src="images/gallery-12.jpg" alt=""/></span></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="clear"></div>
+        </div>
+      </section>  
+    )
+}
