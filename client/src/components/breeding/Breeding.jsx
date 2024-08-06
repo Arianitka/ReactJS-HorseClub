9cdd1a5f2ffd1aa2bceaf884b0e3cadded854e48
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-08-06 12:50:54 +0300

    create breeding folder

---
+++ client/src/components/breeding/Breeding.jsx
@@ -0,0 +1,51 @@
+export default function Breeding(){
+    return (
+        <section id="content">
+  <div className="container_12 top-4">
+    <div className="grid_12">
+      <h2 className="p2">Breeding Stallions</h2>
+      <div className="wrap box-3">
+        <div> <img src="images/page2-img1.jpg" alt=""/>
+          <p className="text-1 top-2 p3">Lorem ipsum</p>
+          <p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo.</p>
+          <a href="#" className="button top-3">Read More</a> </div>
+        <div> <img src="images/page2-img2.jpg" alt=""/>
+          <p className="text-1 top-2 p3">Consetetur sadipscing</p>
+          <p>Duo dolores et ea rebum stet clita kasd gubergren, no sea takimata sanctus est lorem ipsum. dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</p>
+          <a href="#" className="button top-3">Read More</a> </div>
+        <div className="last"> <img src="images/page2-img3.jpg" alt=""/>
+          <p className="text-1 top-2 p3">Diam nonumy</p>
+          <p>Eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+          <a href="#" className="button top-3">Read More</a> </div>
+      </div>
+    </div>
+    <div className="grid_12">
+      <div className="line pad-2"></div>
+    </div>
+    <div className="grid_4">
+      <h2 className="p5">Horse Breeds</h2>
+      <p className="p3">Mata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore:</p>
+      <ul className="list-2">
+        <li><a href="#">Magna aliquyam erat sed diam</a></li>
+        <li><a href="#">At vero eos et accusam</a></li>
+        <li><a href="#">Stet clita kasd gubergren sea takimata</a></li>
+        <li><a href="#">Lorem ipsum dolor sit amet</a></li>
+        <li><a href="#">Consetetur sadipscing elitr sed</a></li>
+        <li><a href="#">Diam nonumy eirmod</a></li>
+      </ul>
+    </div>
+    <div className="grid_8">
+      <div className="left-1">
+        <h2 className="p5">Breeding Stallions</h2>
+        <div className="wrap"> <img src="images/page2-img4.jpg" alt="" className="img-indent-2"/>
+          <p className="clr-1 p3">At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+          <p className="p3">Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ipsum dolor sit amet, consetetur sadipscing elitr.</p>
+          <p>At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+    </div>
+    <div className="clear"></div>
+  </div>
+</section>
+    )
+}
