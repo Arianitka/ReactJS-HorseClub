b1fb5bf25b5fc1c028e760bbde02533163e0b1cc
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-08-06 12:51:10 +0300

    create events page

---
+++ client/src/components/events/Events.jsx
@@ -0,0 +1,71 @@
+export default function Events(){
+    return (
+
+        <section id="content">
+  <div className="container_12 top-4">
+    <div className="grid_4">
+      <h2 className="p5">Latest News</h2>
+      <p><a href="#" className="link">7.05.2012</a></p>
+      <p className="clr-1">Consetetur sadipscing elitr</p>
+      <p className="p3">Sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed <br/>
+        diam voluptua.</p>
+      <p><a href="#" className="link">2.05.2012</a></p>
+      <p className="clr-1">At vero eos et accusam et justo</p>
+      <p className="p3">Duo dolores et ea rebum stet clita kasd gubergren, no sea takimata sanctus est <br/>
+        lorem ipsum. dolor sit amet orem ipsum<br/>
+        dolor.</p>
+      <p><a href="#" className="link">29.04.2012</a></p>
+      <p className="clr-1">Sit amet, consetetur sadips</p>
+      <p className="p3">Cing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+      <p><a href="#" className="link">12.04.2012</a></p>
+      <p className="clr-1">At vero eos et accusam et justo duo</p>
+      <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+      <a href="#" className="button top-3">Read More</a> </div>
+    <div className="grid_8">
+      <div className="left-1">
+        <h2 className="p5">Well Being of Your Horse</h2>
+        <div className="wrap"> <img src="images/page4-img1.jpg" alt="" className="img-indent-2"/>
+          <div className="extra-wrap">
+            <p className="p3"><span className="clr-1">Consetetur sadipscing elitr, sed diam nonumy eirmod</span> tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et.</p>
+            <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.</p>
+          </div>
+        </div>
+        <h2 className="p5 top-5">Facilities at Equestrian Farm</h2>
+        <div className="lists">
+          <ul className="list-2">
+            <li><a href="#">Nam liber tempor cum soluta</a></li>
+            <li><a href="#">Nobis eleifend option congue nihil</a></li>
+            <li><a href="#">Imperdiet doming id quod mazim placerat</a></li>
+            <li><a href="#">Facer possim assum. Lorem ipsum dolor</a></li>
+            <li><a href="#">Sit amet, consectetuer adipiscing</a></li>
+          </ul>
+          <ul className="list-2 last">
+            <li><a href="#">Elit sed diam nonummy nibh euismod</a></li>
+            <li><a href="#">Tincidunt ut laoreet dolore magna aliquam</a></li>
+            <li><a href="#">Ut wisi enim ad minim veniam quis</a></li>
+            <li><a href="#">Nostrud exerci tation ullamcorper suscipit</a></li>
+            <li><a href="#">Lobortis nisl ut aliquip ex ea</a></li>
+          </ul>
+        </div>
+        <h2 className="p5 top-6">Upcoming Events</h2>
+        <div className="wrap p2"> <img src="images/page4-img2.jpg" alt="" className="img-indent-2"/>
+          <div className="extra-wrap">
+            <p><a href="#" className="link">13.05.2012</a></p>
+            <p className="clr-1">Consetetur sadipscing elitr, sed diam nonumy eirmod</p>
+            <p>Tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptu vero eos et accusam et justo duo dolores. Stet clita kasd gubergren, no sea takimata sanctus est.</p>
+          </div>
+        </div>
+        <div className="wrap"> <img src="images/page4-img3.jpg" alt="" className="img-indent-2"/>
+          <div className="extra-wrap">
+            <p><a href="#" className="link">17.05.2012</a></p>
+            <p className="clr-1">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</p>
+            <p>Invidunt ut labore et dolore magna aliquyam erat, sed diamt vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="clear"></div>
+  </div>
+</section>
+    )
+}
