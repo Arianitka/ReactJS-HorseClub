379fec032c2ea142af9d40d9a477882ae6da5b61
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-08-10 15:36:17 +0300

    Add register validation

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,30 +1,81 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useRegister } from "../../hooks/useAuth"
+import { useForm } from "../../hooks/useForm";
 
-export default function Register(){
+const initialValues = { email: '', password: '', 'confirm-password': '' };
+
+export default function Register() {
+    const [error, setError] = useState('')
+    const register = useRegister();
+    const navigate = useNavigate();
+
+    const registerHandler = async (values) => {
+
+        if (values.password !== values['confirm-password']) {
+            return setError('Passwords do not match')
+        }
+        try {
+            await register(values.email,values.password)
+            navigate('/');
+        } catch (err) {
+           setError(err.message)
+        }
+    };
+
+    const {
+        values,
+        changeHandler,
+        submitHandler,
+    } = useForm(initialValues, registerHandler);
+
     return (
         <section id="register-page" className="content auth">
-        <form id="register">
-        <h1>Register</h1>
-            <div className="container">
-                <div className="brand-logo"></div>
-               
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" placeholder="maria@email.com"/>
+            <form id="register" onSubmit={submitHandler}>
+                <h1>Register</h1>
+                <div className="container">
+                    <div className="brand-logo"></div>
 
-                <label htmlFor="pass">Password:</label>
-                <input type="password" name="password" id="register-password"/>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={values.email}
+                        onChange={changeHandler}
+                        placeholder="maria@email.com"
+                    />
 
-                <label htmlFor="con-pass">Confirm Password:</label>
-                <input type="password" name="confirm-password" id="confirm-password"/>
+                    <label htmlFor="pass">Password:</label>
+                    <input 
+                    type="password"
+                     name="password" 
+                     value={values.password}
+                     onChange={changeHandler}
+                     id="register-password" />
 
-                <input className="btn submit" type="submit" value="Register"/>
+                    <label htmlFor="con-pass">Confirm Password:</label>
+                    <input
+                     type="password"
+                      name="confirm-password"
+                       id="confirm-password"
+                       value={values['confirm-password']}
+                       onChange={changeHandler}
+                        />
 
-                <p className="field">
-                    <span>If you already have profile click <Link to={`/login`}>here</Link></span>
-                </p>
-            </div>
-        </form>
-    </section>
+                        {error && (
+                            <p >
+                            <span>{error}</span>
+                        </p>
+                        )}
+
+                    <input className="btn submit" type="submit" value="Register" />
+                    
+                    <p className="field">
+                        <span>If you already have profile click <Link to={`/login`}>here</Link></span>
+                    </p>
+                </div>
+            </form>
+        </section>
 
     )
 }
--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -9,7 +9,24 @@ export const useLogin = () => {
     const loginHandler = async (email, password) => {
         const result = await login(email, password);
         changeAuthState(result);
+
+        return result;
        
     }
     return loginHandler;
 };
+
+export const useRegister = () => {
+    const {changeAuthState} = useContext(AuthContext);
+
+    const registerHandler = async (email, password) => {
+        const result = await register(email, password);
+
+        changeAuthState(result);
+
+        return result;
+
+    }
+
+    return registerHandler;
+}
