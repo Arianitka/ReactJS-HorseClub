6a1bdd9cb3c150dfed286278b6e36afa1ccb192e
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-08-13 02:02:32 +0300

    added Profile page

---
+++ client/src/components/profile/Profile.jsx
@@ -0,0 +1,29 @@
+export default function Profile (){
+   
+    return (
+
+        <main className="cd__main">
+       
+        <div className="person">
+          <figure className="person__photo">
+            {/* <img src="./src/images/istockphoto-1495088043-612x612.jpg" alt="Jane Doe"/> */}
+          </figure>
+          <header className="person__header">
+            <h1 className="person__name">Jane Doe</h1>
+            <div className="person__title">Graphic Designer</div>
+          </header>
+          <div className="darken"></div>
+          <main className="person__main">
+            <p className="person__bio">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, accusamus maxime ipsa iusto facilis illum autem officia quidem enim earum, quo necessitatibus? Nobis saepe sint dicta distinctio quos repellat.</p>
+          </main>
+          
+          <div className="background"></div>
+        </div>
+       
+               
+    </main>      
+        
+  
+    )
+  }
+     
